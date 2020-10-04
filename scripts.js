bd4131aa6706fc89e2658004960b958b37ff0ce1
Dan Wagner <danwagnerwd@gmail.com>
2020-10-04 10:54:27 -0400

    Finished.

--- scripts.js
+++ scripts.js
@@ -3,64 +3,72 @@ const background = document.querySelector('body');
 const btn = document.querySelector('#buttonBackground');
 const header = document.querySelector('h1');
 
-btn.addEventListener('click', () => {
-  const colors = ['#311D3F', '#522546', '#88304E', '#E23E57', '#FFFFFF'];
-  let randomColor = Math.floor(Math.random() * colors.length);
-  background.style.backgroundColor = colors[randomColor];
-});
+if (btn) {
+  btn.addEventListener('click', () => {
+    const colors = ['#311D3F', '#522546', '#88304E', '#E23E57', '#FFFFFF'];
+    let randomColor = Math.floor(Math.random() * colors.length);
+    background.style.backgroundColor = colors[randomColor];
+  });
+}
 
 // Letter Counter
 const inputField = document.querySelector('#inputField');
 let counter = document.querySelector('#outputText');
-counter.innerText = 0;
-inputField.value = '';
+if (counter) {
+  counter.innerText = 0;
+  inputField.value = '';
 
-inputField.addEventListener('keyup', () => {
-  counter.innerText = inputField.value.length;
-});
+  inputField.addEventListener('keyup', () => {
+    counter.innerText = inputField.value.length;
+  });
+}
 
 // Counter
 const currentCount = document.querySelector('#currentCount');
 let countNumber = 0;
 const decrease = document.querySelector('#decreaseButton');
 const increase = document.querySelector('#increaseButton');
-increase.addEventListener('click', () => {
-  countNumber++;
-  currentCount.innerHTML = countNumber;
-});
-decrease.addEventListener('click', () => {
-  countNumber--;
-  currentCount.innerHTML = countNumber;
-});
+if (increase) {
+  increase.addEventListener('click', () => {
+    countNumber++;
+    currentCount.innerHTML = countNumber;
+  });
+  decrease.addEventListener('click', () => {
+    countNumber--;
+    currentCount.innerHTML = countNumber;
+  });
+}
 
 // To Do List
 const createListItem = document.querySelector('#toDoBtn');
 const createListField = document.querySelector('#listFields');
 const addItem = document.querySelector('#childContainer');
 
-createListItem.addEventListener('click', () => {
-  const addItemContainer = document.createElement('div');
-  const listItem = document.createElement('input');
-  const removeBtn = document.createElement('button');
+if (createListItem) {
+  createListItem.addEventListener('click', () => {
+    const addItemContainer = document.createElement('div');
+    const listItem = document.createElement('input');
+    const removeBtn = document.createElement('button');
 
-  addItemContainer.className = 'addContainer';
-  listItem.className = 'toDoField';
-  removeBtn.className = 'removeItemBtn';
-  removeBtn.innerText = 'Remove';
+    addItemContainer.className = 'addContainer';
+    listItem.className = 'toDoField';
+    removeBtn.className = 'removeItemBtn';
+    removeBtn.innerText = 'Remove';
 
-  addItem.appendChild(addItemContainer);
-  addItemContainer.appendChild(listItem);
-  addItemContainer.appendChild(removeBtn);
-  listItem.value = createListField.value;
-  createListField.value = '';
+    addItem.appendChild(addItemContainer);
+    addItemContainer.appendChild(listItem);
+    addItemContainer.appendChild(removeBtn);
+    listItem.value = createListField.value;
+    createListField.value = '';
 
-  const itemRemoveBtn = document.querySelectorAll('.removeItemBtn');
-  if (itemRemoveBtn[0] !== null) {
-    const itemContainer = document.querySelectorAll('.addContainer');  
-    for (let i = 0; i < itemRemoveBtn.length; i++) {      
-      itemRemoveBtn[i].addEventListener('click', () => {
-        itemContainer[i].remove();
-      });
-    }
-  }       
-});
+    const itemRemoveBtn = document.querySelectorAll('.removeItemBtn');
+    if (itemRemoveBtn[0]) {
+      const itemContainer = document.querySelectorAll('.addContainer');  
+      for (let i = 0; i < itemRemoveBtn.length; i++) {      
+        itemRemoveBtn[i].addEventListener('click', () => {
+          itemContainer[i].remove();
+        });
+      }
+    }       
+  });
+}
