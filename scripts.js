33619a17eb52c14db115b57e4c41b502bc1800f7
Dan Wagner <danwagnerwd@gmail.com>
2020-11-16 22:32:44 -0500

    Add gitignore containing node_modules. Re-factor JS

--- scripts.js
+++ scripts.js
@@ -1,34 +1,27 @@
 // Change Background
-const background = document.querySelector('body');
-const btn = document.querySelector('#buttonBackground');
-const header = document.querySelector('h1');
-
-if (btn) {
+function changeBackground(btn) {
   btn.addEventListener('click', () => {
     const colors = ['#311D3F', '#522546', '#88304E', '#E23E57', '#FFFFFF'];
     let randomColor = Math.floor(Math.random() * colors.length);
-    background.style.backgroundColor = colors[randomColor];
+    document.querySelector('body').style.backgroundColor = colors[randomColor];
   });
 }
 
 // Letter Counter
-const inputField = document.querySelector('#inputField');
-let counter = document.querySelector('#outputText');
-if (counter) {
+function count(counter) {
+  const inputField = document.querySelector('#inputField');
   counter.innerText = 0;
   inputField.value = '';
-
   inputField.addEventListener('keyup', () => {
     counter.innerText = inputField.value.length;
   });
 }
 
 // Counter
-const currentCount = document.querySelector('#currentCount');
-let countNumber = 0;
-const decrease = document.querySelector('#decreaseButton');
-const increase = document.querySelector('#increaseButton');
-if (increase) {
+function countNumber(currentCount) {
+  let countNumber = 0;
+  const decrease = document.querySelector('#decreaseButton');
+  const increase = document.querySelector('#increaseButton');
   increase.addEventListener('click', () => {
     countNumber++;
     currentCount.innerHTML = countNumber;
@@ -40,12 +33,10 @@ if (increase) {
 }
 
 // To Do List
-const createListItem = document.querySelector('#toDoBtn');
-const addToYourList = document.querySelector('#listFields');
-const childContainer = document.querySelector('#childContainer');
-
-if (createListItem) {
+function listItem(createListItem) {
+  const addToYourList = document.querySelector('#listFields');
   createListItem.addEventListener('click', () => {
+    const childContainer = document.querySelector('#childContainer');
     const removeListContainer = document.createElement('div');
     const childListItem = document.createElement('input');
     const removeBtn = document.createElement('button');
